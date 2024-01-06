ff6b45780861337ab2cef0fc9721d42133dca663
Th3-Coder <152851285+Th3-C0der@users.noreply.github.com>
2024-01-06 16:58:23 +0530

    Update .update.php

--- modules/.update.php
+++ modules/.update.php
@@ -28,8 +28,8 @@ function upd() {
   logo();
   echo "\n\033[01;32mUpdating IP-Tracer.........\033[01;37m\n\n";
   sleep(1);
-  system("cd ~/ && git clone https://github.com/Th3Coder8/IP-Tracer.git");
-  system("cd ~/ && sudo git clone https://github.com/Th3Coder8/IP-Tracer.git");
+  system("cd ~/ && git clone https://github.com/Th3-C0der/IP-Tracer.git");
+  system("cd ~/ && sudo git clone https://github.com/Th3-C0der/IP-Tracer.git");
   system("cd ~/IP-Tracer && sh install");
   logo();
   echo "\n\033[01;32m              IP-Tracer updated !!!\033[01;37m\n";
